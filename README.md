5d85257d9d1d704891cd1f330af182604b9c7161
Roshan Shrestha <roshanshrestha01@gmail.com>
2020-04-19 10:11:16 +0545

    Added README.md and using registry module

---
+++ README.md
@@ -0,0 +1,27 @@
+# Terraform build up
+
+Project utilized [registry](https://registry.terraform.io/) modules to build infrastructure
+
+## Setting up terraform
+
+* Download terraform from [here](https://www.terraform.io/downloads.html).
+* Unzip downloaded file
+* Move unzip file `sudo mv terraform /usr/local/bin/`
+
+
+## Setting aws credentials
+
+```sh
+$ export AWS_ACCESS_KEY_ID="<ACCESS_KEY_ID>"
+$ export AWS_SECRET_ACCESS_KEY="<SECRET_ACCESS_KEY>"
+$ export AWS_DEFAULT_REGION="<DEFAULT_REGION>"
+```
+
+## Terrafrom build
+
+```sh
+$ terraform init
+$ terraform plan
+$ terraform apply
+```
+
--- main.tf
+++ main.tf
@@ -1,4 +1,24 @@
 module "vpc" {
-  source = "./modules/vpc/"
-  environment = "production"
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "accounting-box-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2"]
+  public_subnets  = ["10.0.0.0/24"]
+  private_subnets = ["10.0.1.0/24"]
+  
+  enable_nat_gateway = true
+  single_nat_gateway = false
+  enable_vpn_gateway = true
+
+  create_database_subnet_route_table=false
+  create_redshift_subnet_route_table=false
+  create_elasticache_subnet_route_table=false
+  
+
+  tags = {
+    Terraform = "true"
+    Environment = "staging"
+  }
 }
