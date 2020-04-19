fbcadeeee19eea081fd5fa9f67f28c03aa7b0795
Roshan Shrestha <roshanshrestha01@gmail.com>
2020-04-19 11:12:40 +0545

    Change VPC ip to 10.0.0.0/16 to 10.1.0.0/16

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "accounting-box-vpc-staging"
-  cidr = "10.0.0.0/16"
+  cidr = "10.1.0.0/16"
   enable_ipv6 = false
   azs             = ["us-west-2"]
-  public_subnets  = ["10.0.0.0/24"]
-  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.1.0.0/24"]
+  private_subnets = ["10.1.1.0/24"]
   
   enable_nat_gateway = true
   single_nat_gateway = false
